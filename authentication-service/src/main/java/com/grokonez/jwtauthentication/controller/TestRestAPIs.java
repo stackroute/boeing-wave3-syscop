3b739717a8c64e1c1e80dec77806748efd7d35df
Rohith.Prakash <rohith.prakash@boeing.com>
2019-02-15 10:10:59 +0530

    authentication service commit1

---
+++ authentication-service/src/main/java/com/grokonez/jwtauthentication/controller/TestRestAPIs.java
@@ -0,0 +1,18 @@
+package com.grokonez.jwtauthentication.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+public class TestRestAPIs {
+	
+	@GetMapping("/api/test/user")
+	//@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public String userAccess() {
+		return ">>> User Contents!";
+	}
+
+}
---
+++ authentication-service/src/test/java/com/grokonez/jwtauthentication/controller/AuthRestAPIsTest.java
@@ -0,0 +1,7 @@
+package com.grokonez.jwtauthentication.controller;
+
+import static org.junit.Assert.*;
+
+public class AuthRestAPIsTest {
+
+}
