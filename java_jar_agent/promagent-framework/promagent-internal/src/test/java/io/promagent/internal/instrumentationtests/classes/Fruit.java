633ba10864cc76a17153b757c80e6b4890e13cb2
anu <anusaini16394@gmail.com>
2019-02-15 16:02:25 +0530

    agent

--- java_jar_agent/promagent-framework/promagent-internal/src/test/java/io/promagent/internal/instrumentationtests/classes/Fruit.java
+++
@@ -1,8 +0,0 @@
-package io.promagent.internal.instrumentationtests.classes;
-
-/**
- * Example of object oriented inheritance.
- */
-public class Fruit {
-    public static class Orange extends Fruit{}
-}
