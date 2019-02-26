16710bfc997f43510436d65033b71d04617bd2da
Vaibhav AGochiya <vaibhav1302@git.com>
2019-02-26 12:27:00 +0530

    Adding code for job queue

--- dataCollectorFetcher/src/main/java/com/anjali/dataCollector/ThreadManager/JobModel.java
+++ dataCollectorFetcher/src/main/java/com/anjali/dataCollector/ThreadManager/JobModel.java
@@ -1,4 +1,31 @@
 package com.anjali.dataCollector.ThreadManager;
 
 public class JobModel {
+    private String userId;
+    private String agentType;
+    private String agentAPI;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getAgentType() {
+        return agentType;
+    }
+
+    public void setAgentType(String agentType) {
+        this.agentType = agentType;
+    }
+
+    public String getAgentAPI() {
+        return agentAPI;
+    }
+
+    public void setAgentAPI(String agentAPI) {
+        this.agentAPI = agentAPI;
+    }
 }
--- dataCollectorFetcher/src/main/java/com/anjali/dataCollector/ThreadManager/JobQueue.java
+++ dataCollectorFetcher/src/main/java/com/anjali/dataCollector/ThreadManager/JobQueue.java
@@ -2,6 +2,28 @@ package com.anjali.dataCollector.ThreadManager;
 
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 @Component
 public class JobQueue {
+
+    private Queue<JobModel> queue = new LinkedList<>();
+
+    public void addJob(JobModel jobModel){
+
+        queue.add(jobModel);
+
+    }
+
+    public void popJob(JobModel jobModel){
+
+        queue.remove(jobModel);
+
+    }
+
+    public int getSize(){
+        return queue.size();
+    }
+
 }
