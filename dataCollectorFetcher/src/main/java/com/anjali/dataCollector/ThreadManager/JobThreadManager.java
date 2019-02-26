613ed27fd4d4dbf8c38f037a282cc103b85be89c
Vaibhav AGochiya <vaibhav1302@git.com>
2019-02-26 12:51:18 +0530

    Adding code for job queue

--- dataCollectorFetcher/src/main/java/com/anjali/dataCollector/ThreadManager/JobThreadManager.java
+++ dataCollectorFetcher/src/main/java/com/anjali/dataCollector/ThreadManager/JobThreadManager.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobThreadManager {
     private int maxThread = 2;
+    private int threadCount = 0;
     private JobQueue jobQueue;
 
     @Autowired
     public JobThreadManager(JobQueue jobQueue) {
         this.jobQueue = jobQueue;
+    }
+
+    public int getThreadCount() {
+        return threadCount;
+    }
+
+    public void setThreadCount(int threadCount) {
+        this.threadCount = threadCount;
     }
 }
