012fc3fed154f34a3d8c92c12ee6562cd1a3be84
Vaibhav AGochiya <vaibhav1302@git.com>
2019-02-27 10:08:51 +0530

    Adding Collector Code

--- dataCollectorFetcher/src/main/java/com/anjali/dataCollector/KafkaService/KafkaListenerService.java
+++ dataCollectorFetcher/src/main/java/com/anjali/dataCollector/KafkaService/KafkaListenerService.java
@@ -1,15 +1,28 @@
 package com.anjali.dataCollector.KafkaService;
 
 
+import com.anjali.dataCollector.ThreadManager.JobModel;
+import com.anjali.dataCollector.ThreadManager.JobQueue;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaListenerService {
 
+    private JobQueue jobQueue;
+    private JobModel jobModel;
+
+    @Autowired
+    public KafkaListenerService(JobQueue jobQueue) {
+        this.jobQueue = jobQueue;
+    }
+
     @KafkaListener(topics = "kafkaAppRegistration", groupId = "group_id2")
     public void consume(String message) throws JsonProcessingException {
         System.out.println("Consumed msg : " + message);
+        //Create job in jobqueue
+//        jobQueue.addJob(jobModel);
     }
 }
