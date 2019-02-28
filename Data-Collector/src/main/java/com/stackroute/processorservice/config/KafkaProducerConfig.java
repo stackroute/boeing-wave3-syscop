58bc144da08ad1e88383ec37f8369e1f21ab7ca7
avinashkpai12 <avinashkpai12@gmail.com>
2019-02-28 11:23:34 +0530

    added mongo query

--- Data-Collector/src/main/java/com/stackroute/processorservice/config/KafkaProducerConfig.java
+++ Data-Collector/src/main/java/com/stackroute/processorservice/config/KafkaProducerConfig.java
@@ -18,7 +18,7 @@ public class KafkaProducerConfig {
         public ProducerFactory<String, String> producerFactory() {
             Map<String, Object> config = new HashMap<>();
 
-            config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.85:9092");
+            config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.151:9092");
             config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
             config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
--- Data-Collector/src/main/java/com/stackroute/processorservice/repository/DataCollectorRepository.java
+++ Data-Collector/src/main/java/com/stackroute/processorservice/repository/DataCollectorRepository.java
@@ -1,4 +1,14 @@
 package com.stackroute.processorservice.repository;
 
-public interface DataCollectorRepository {
+import com.stackroute.processorservice.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DataCollectorRepository extends MongoRepository<User, String> {
+
+    @Query("{'applications.services.portNumber': ?0}")
+    public List<User> findUser(int portNumber);
 }
