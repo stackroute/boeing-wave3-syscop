eb578c77c70e6855f1e3c5dc7048c364eb75316c
ShriRamyaA <rammiu96@gmail.com>
2019-02-28 15:09:19 +0530

    Update KafkaConfiguration.java

--- AppRegistration/src/main/java/com/stackroute/AppRegistration/config/KafkaConfiguration.java
+++ AppRegistration/src/main/java/com/stackroute/AppRegistration/config/KafkaConfiguration.java
@@ -28,7 +28,7 @@ public class KafkaConfiguration {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.232.165.99:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
