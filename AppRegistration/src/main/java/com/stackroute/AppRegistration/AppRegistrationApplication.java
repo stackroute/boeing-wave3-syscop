d9d32d4e198c407b272dab4f99ba161ac2c3624c
ShriRamyaA <rammiu96@gmail.com>
2019-03-01 09:51:33 +0530

    Update AppRegistrationApplication.java

--- AppRegistration/src/main/java/com/stackroute/AppRegistration/AppRegistrationApplication.java
+++ AppRegistration/src/main/java/com/stackroute/AppRegistration/AppRegistrationApplication.java
@@ -2,13 +2,9 @@ package com.stackroute.AppRegistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-
-=======
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
->>>>>>> d4893e1a6661654a84f70f0dcbd23bbe66c4302d
 @SpringBootApplication
 public class AppRegistrationApplication {
 
