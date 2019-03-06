9e9d239b635dba20dee946290a6adf3f1b43d703
Shri <rammiu96@gmail.com>
2019-03-06 19:04:57 +0530

    monitoring-service update

--- monitoring-service/src/main/java/com/stackroute/monitorservice/MonitorServiceApplication.java
+++ monitoring-service/src/main/java/com/stackroute/monitorservice/MonitorServiceApplication.java
@@ -2,7 +2,9 @@ package com.stackroute.monitorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MonitorServiceApplication {
 
