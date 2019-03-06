d4f4e3390c0b9215894a8e3e39c0e4bcc7e00805
Rohith.Prakash <rohith.prakash@boeing.com>
2019-03-06 16:57:15 +0530

    processor service update

--- processor-service/src/main/java/com/stackroute/processorservice/ProcessorServiceApplication.java
+++ processor-service/src/main/java/com/stackroute/processorservice/ProcessorServiceApplication.java
@@ -3,7 +3,9 @@ package com.stackroute.processorservice;
 import com.stackroute.processorservice.service.InfluxServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProcessorServiceApplication {
 
--- processor-service/src/main/java/com/stackroute/processorservice/config/KafkaProducerConfig.java
+++ processor-service/src/main/java/com/stackroute/processorservice/config/KafkaProducerConfig.java
@@ -20,7 +20,7 @@ public class KafkaProducerConfig {
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.23.239.151:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
