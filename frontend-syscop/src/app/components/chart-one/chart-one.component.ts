92ad92f92a43ccdb087410e9d483e201b8a22053
sushantanshu <anshu.sushant@gmail.com>
2019-03-08 12:39:00 +0530

    frontend

--- frontend-syscop/src/app/components/chart-one/chart-one.component.ts
+++ frontend-syscop/src/app/components/chart-one/chart-one.component.ts
@@ -85,7 +85,7 @@ ngAfterViewInit() {
   const myLine = new Chart(ctx, this.config);
 
   /* Configuring WebSocket on Client Side */
-  const socket = new SockJS('http://13.232.165.99:8095/monitoring-service/live-metrics');
+  const socket = new WebSocket('ws://13.232.165.99:8095/monitoring-service/live-metrics');
   this.stompClient = Stomp.over(socket);
   this.stompClient.connect({}, function (frame) {
     that.stompClient.subscribe('/topic/cpu-metrics', function (temperature) {
