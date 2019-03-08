f38b96b6441460d2e12b8e551223c36fcdbb07f7
Vaibhav AGochiya <vaibhav1302@git.com>
2019-03-08 09:58:57 +0530

    Updating Agent Loader

--- agentloader/src/main/java/Main.java
+++ agentloader/src/main/java/Main.java
@@ -15,12 +15,13 @@ public class Main {
             try{
                 VirtualMachine vm0 = VirtualMachine.attach(vm.id());
                 // load agent, agnet class agentmain will be invoked.
-                vm0.loadAgent(args[0], "port="+ Integer.toString(port++));
-                System.out.println("Load agent done.");
+                vm0.loadAgent(args[0], "port="+ Integer.toString(port));
+                System.out.println("Load agent done at port: "+port);
                 vm0.detach();
             }catch(Exception e) {
                 System.out.println("exception : " + e.getMessage());
             }
+            port++;
         }
 
     }
