bd2b178120536d87bb034a64b016b38eab81c50e
avinashkpai12 <avinashkpai12@gmail.com>
2019-03-11 16:21:30 +0530

    KafkaConfig changed

--- monitoring-service/src/main/java/com/stackroute/monitorservice/config/KafkaConfiguration.java
+++ monitoring-service/src/main/java/com/stackroute/monitorservice/config/KafkaConfiguration.java
@@ -19,8 +19,8 @@ public class KafkaConfiguration {
     public ConsumerFactory<String,String> consumerFactory(){
         Map<String,Object> config = new HashMap<>();
 
-//        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
-         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.23.239.151:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+//         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.23.239.151:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id_monitoring");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
