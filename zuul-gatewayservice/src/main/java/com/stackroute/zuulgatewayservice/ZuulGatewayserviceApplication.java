04a49659d8132f84991836730843a791e28223da
avinashkpai12 <avinashkpai12@gmail.com>
2019-03-14 09:44:55 +0530

    added https

--- zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/ZuulGatewayserviceApplication.java
+++ zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/ZuulGatewayserviceApplication.java
@@ -1,17 +1,35 @@
 package com.stackroute.zuulgatewayservice;
 
+//import com.stackroute.zuulgatewayservice.config.HttpRedirect;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+
 
 @CrossOrigin("*")
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulGatewayserviceApplication {
+
+
+
+//	@Bean
+//	public HttpRedirect httpRedirect1() {
+//		return new HttpRedirect();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulGatewayserviceApplication.class, args);
@@ -32,5 +50,8 @@ public class ZuulGatewayserviceApplication {
 	public RouteFilter routeFilter() {
 		return new RouteFilter();
 	}
+
+
+
 
 }
---
+++ zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/config/HttpRedirect.java
@@ -0,0 +1,51 @@
+package com.stackroute.zuulgatewayservice.config;
+
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpRedirect {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    private static final String USER_CONSTRAINT = "CONFIDENTIAL";
+    private static final String HTTP = "http";
+    private static final int HTTPS_PORT = 8095  ;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint(USER_CONSTRAINT);
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(
+                TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme(HTTP);
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(HTTPS_PORT);
+        return connector;
+    }
+
+}
