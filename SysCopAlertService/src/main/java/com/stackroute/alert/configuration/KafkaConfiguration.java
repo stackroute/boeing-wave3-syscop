a8d4374e4d7bca0d86a6a64e740c08ac62268227
Durgesh Kumar <durgeshkumar0895@gmail.com>
2019-03-19 10:47:51 +0530

    Update KafkaConfiguration.java
    kafka IP changed

--- SysCopAlertService/src/main/java/com/stackroute/alert/configuration/KafkaConfiguration.java
+++ SysCopAlertService/src/main/java/com/stackroute/alert/configuration/KafkaConfiguration.java
@@ -48,7 +48,7 @@ public class KafkaConfiguration {
     public ConsumerFactory<String,String> consumerFactoryAppRegistration(){
         Map<String,Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.23.239.151:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id2");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class);
