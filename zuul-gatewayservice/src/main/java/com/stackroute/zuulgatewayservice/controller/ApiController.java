e909600ef19313af05d9020a791cffbefeb90a96
avinashkpai12 <avinashkpai12@gmail.com>
2019-03-20 19:11:15 +0530

    name changed

--- zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/controller/ApiController.java
+++ zuul-gatewayservice/src/main/java/com/stackroute/zuulgatewayservice/controller/ApiController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
-public class ZuulController {
+public class ApiController {
 
     @GetMapping("zuul")
     public ResponseEntity<?> test() {
